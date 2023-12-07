7778cdc5820c8ad91a1b2e51ef582cbdbcf4d99d
venky1306 <47674519+venky1306@users.noreply.github.com>
2023-12-06 21:58:13 -0600

    get inventory.php

---
+++ get_inventory.php
@@ -0,0 +1,42 @@
+<?php
+
+$hostname = '128.199.7.149';
+$username = 'root';
+$password = 'change-me';
+$database = 'nile';
+
+// Connect to the database
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check for a successful connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Select all inventory data from the Inventory table
+$query = "SELECT * FROM Inventory";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    $inventoryData = array();
+
+    // Fetch each row from the result and store it in $inventoryData array
+    while ($row = $result->fetch_assoc()) {
+        $inventoryData[] = $row;
+    }
+
+    // Convert the $inventoryData array to JSON format
+    $jsonResponse = json_encode($inventoryData);
+
+    // Set headers to indicate JSON content type
+    header('Content-Type: application/json');
+
+    // Output the JSON response
+    echo $jsonResponse;
+} else {
+    echo "No inventory data found.";
+}
+
+// Close the database connection
+$conn->close();
+?>
